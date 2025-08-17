4bf62412cb52e399b2993429a7d29599a5099c0f
Marco Garagna <marcogara24@gmail.com>
2025-08-17 11:41:29 +0000

    lets check if some file of the car project is missing and check teh link

---
+++ public/carProject/controls.js
@@ -0,0 +1,55 @@
+class Controls{
+    constructor(type){
+        this.forward=false;
+        this.reverse=false;
+        this.right=false;
+        this.left=false;
+
+        switch(type){
+            case "KEYS":
+                this.#addKeyboardListeners();
+                break;
+                case "DUMMY":
+                    this.forward=true;
+                    break;    
+        }
+    }
+
+    #addKeyboardListeners(){
+        document.onkeydown=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=true;
+                    break;
+                case "ArrowRight":
+                    this.right=true;
+                    break;    
+                case "ArrowUp":
+                    this.forward=true;
+                    event.preventDefault(); // Prevent default behavior (scrolling)
+                    break;
+                case "ArrowDown":
+                    this.reverse=true;
+                    event.preventDefault(); // Prevent default behavior (scrolling)
+                    break;    
+            }
+        }
+        document.onkeyup=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=false;
+                    break;
+                case "ArrowRight":
+                    this.right=false;
+                    break;    
+                case "ArrowUp":
+                    this.forward=false;
+                    break;
+                case "ArrowDown":
+                    this.reverse=false;
+                    break;    
+            }
+        }
+    }
+    
+}
